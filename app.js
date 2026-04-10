0ea8cdc57e7261152369f1176adaaa916abd89a4
alinechapini <alinechapini@gmail.com>
2026-04-10 19:25:39 -0300

    Refactor variable types and improve alert messages

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 alert ('Boas Vindas ao nosso site');
 let nome = "Lua";
-let idade = "25";
-let NumeroDeVenas = "50";
-let SaldoDisponivel = "1000";
+let idade = 25;
+let NumeroDeVendas = 50;
+let SaldoDisponivel = 1000;
 alert ('Erro! Preencha todos os campos!');
-let mensagemDeErro = ('Erro! Preencha todos os campos!');
+let mensagemDeErro = 'Erro! Preencha todos os campos!';
 alert (mensagemDeErro);
-let nome2 = prompt ('Qual é o seu nome?');
-let idade2 = prompt ('Qual é a sua idade?');
+let nome2 = prompt ('Digite o seu nome');
+let idade2 = prompt ('Digite a sua idade');
 if (idade2 >= 18) {
-    alert ('Pode tirar a habilitação');
+    alert ('Pode tirar a habilitação!');
 }
 
