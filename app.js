91b267403eab32749c969452ed196d9da9e0611b
alinechapini <alinechapini@gmail.com>
2026-04-10 22:19:59 +0000

    desafio-concluido

---
+++ app.js
@@ -0,0 +1,14 @@
+alert ('Boas Vindas ao nosso site');
+let nome = "Lua";
+let idade = "25";
+let NumeroDeVenas = "50";
+let SaldoDisponivel = "1000";
+alert ('Erro! Preencha todos os campos!');
+let mensagemDeErro = ('Erro! Preencha todos os campos!');
+alert (mensagemDeErro);
+let nome2 = prompt ('Qual é o seu nome?');
+let idade2 = prompt ('Qual é a sua idade?');
+if (idade2 >= 18) {
+    alert ('Pode tirar a habilitação');
+}
+
